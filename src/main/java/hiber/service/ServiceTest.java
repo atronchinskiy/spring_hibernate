3a466c64105f0e3badebafb219ae15cf69dd4e87
tab65 <tab65@komus.net>
2020-06-01 12:14:41 +0300

    test h2db

---
+++ src/main/java/hiber/service/ServiceTest.java
@@ -0,0 +1,25 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.cfg.Configuration;
+
+
+public class ServiceTest {
+
+    private static final String hibernate_show_sql = "true";
+    private static final String hibernate_hbm2ddl_auto = "create";
+
+    private Configuration getH2Configuration() {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:./h2db");
+        configuration.setProperty("hibernate.connection.username", "tully");
+        configuration.setProperty("hibernate.connection.password", "tully");
+        configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
+        configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
+        return configuration;
+    }
+}
