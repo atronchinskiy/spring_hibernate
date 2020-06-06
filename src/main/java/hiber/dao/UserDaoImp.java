8403f3feba9e152163ba4b086bc08348cc522f10
tab65 <tab65@komus.net>
2020-06-06 20:50:34 +0300

    changed getCurrentSession on openSession

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -2,7 +2,9 @@ package hiber.dao;
 
 import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,51 +17,151 @@ public class UserDaoImp implements UserDao {
 
     @Autowired
     private SessionFactory sessionFactory;
+    private Session session;
 
     @Override
     public void clearAll() {
-        Query query = sessionFactory.getCurrentSession().createQuery("DELETE User");
-        query.executeUpdate();
-        query = sessionFactory.getCurrentSession().createQuery("DELETE Car");
-        query.executeUpdate();
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Query query;
+            query = session.createQuery("DELETE User");
+            query.executeUpdate();
+            query = session.createQuery("DELETE Car");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void add(User user) {
-        sessionFactory.getCurrentSession().save(user);
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User");
-        return query.getResultList();
+        Transaction transaction = null;
+        List<User> users = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            TypedQuery<User> query = session.createQuery("FROM User");
+            users = query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+            return users;
+        }
     }
 
     public void addCar(Car car) {
-        sessionFactory.getCurrentSession().save(car);
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(car);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     public List<Car> listCar() {
-        TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("FROM Car");
-        return query.getResultList();
+        Transaction transaction = null;
+        List<Car> cars = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            TypedQuery<Car> query = session.createQuery("FROM Car");
+            cars = query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+            return cars;
+        }
     }
 
     public List<Car> listCars() {
-        String select = "SELECT c FROM Car c INNER JOIN User u ON c.userID=u.id";
-        Query query = sessionFactory.getCurrentSession().createQuery(select);
-        return query.getResultList();
+        Transaction transaction = null;
+        List<Car> cars = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            TypedQuery<Car> query = session.createQuery("SELECT c FROM Car c INNER JOIN User u ON c.userID=u.id");
+            cars = query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+            return cars;
+        }
     }
 
     @Override
     public User getUser(String name, int series) {
         User user = null;
-        Query query = sessionFactory.getCurrentSession().createQuery(
-                "SELECT u FROM User u INNER JOIN Car c ON c.userID = u.id and c.name = " + name + "and c.series =" + series);
-        List<User> users = query.list();
-        if (!users.isEmpty()) {
-            user = users.get(0);
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Query query = session.createQuery(
+                "SELECT u FROM User u INNER JOIN Car c ON c.userID = u.id and c.name = :name and c.series = :series");
+            query.setParameter("name", name);
+            query.setParameter("series", series);
+            List<User> users = query.list();
+            transaction.commit();
+            if (!users.isEmpty()) {
+                user = users.get(0);
+            }
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+            return user;
         }
-        return user;
     }
 }
